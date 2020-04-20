92c0d451e184d816251a20be2249cfa49cc05456
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-20 12:51:03 -0500

    add widget url

---
+++ tomcat/models/contact/customer_segment.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+import logging
+from datetime import datetime
+_logger = logging.getLogger(__name__)
+from odoo.tools.misc import clean_context
+from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
+class TomCatCustomerSegment(models.Model):
+   _name = 'tomcat.customer.segment'
+   _inherit =  ['mail.thread', 'mail.activity.mixin']
+   _rec_name = 'name'
+   name = fields.char("Nombre")

889af0f3b717d8db7d7a50d4a8fef3878a4fca30
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-20 10:27:23 -0500

    first push

---
+++ tomcat/models/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#from . import models
+#SALES
+#from  .sales import product__template
+#from  .sales import sale_order_line
+#from  .sales import sale_order
+#from  .sales.reports import sale_report
+#Mail
+#from .mail import mail_activity
+#CRM
+#from .crm import crm_lead
+from .contact import contact
+#from .users import res_user
+#STOCK
+#from .stock_picking import stock_picking
+
+

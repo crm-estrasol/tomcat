995336b9a40a93bc8efc223e076d5d1d2608acd6
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-20 17:41:16 -0500

    cust seg

--- tomcat/__manifest__.py
+++ tomcat/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','product','mail','web','crm','sale_crm','stock'],
+    'depends': ['base','sale','contacts','product','mail','web','crm','sale_crm','stock'],
 
     # always loaded
     'data': [

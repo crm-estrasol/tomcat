d78b9e1421218636540a5b32ad70426cc0cfdaad
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-20 17:48:36 -0500

    cust seg

--- tomcat/models/contact/contact.py
+++ tomcat/models/contact/contact.py
@@ -9,6 +9,8 @@ class TomCatResPartner(models.Model):
     _inherit  = "res.partner"
     external_id = fields.Char('ID externo',tracking=True)
     url_map = fields.Char('Enlace mapa',tracking=True)
+    customer_segments = fields.Many2one("tomcat.customer.segment", string="Segmento cliente",track_visibility=True)
+    intermediary_category = fields.Many2one("tomcat.intermediary.category", string="Categoria intermediario",track_visibility=True)
     #ON BUTTON ACTIONS
 
     #ON COMPUTE

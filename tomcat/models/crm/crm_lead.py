430d1d91d3155c89e884349a0a81a23326db6773
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-21 16:36:20 -0500

    config 3

--- tomcat/models/crm/crm_lead.py
+++ tomcat/models/crm/crm_lead.py
@@ -19,6 +19,6 @@ class TomCatCrmLead(models.Model):
     def _compute_show_light(self):
        for record in self:
             rand  =  random.randint(1,3)
-            record.light_help = 
+            record.light_help = rand
             record.write({'light':rand})
         

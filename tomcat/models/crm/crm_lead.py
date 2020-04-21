1e1b80c7bdd214d7e3ff63f7a74f230e6a17790f
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-21 15:12:56 -0500

    config 3

--- tomcat/models/crm/crm_lead.py
+++ tomcat/models/crm/crm_lead.py
@@ -8,8 +8,8 @@ from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationE
 import random
 class TomCatCrmLead(models.Model):
     _inherit  = "crm.lead"
-    light = fields.Integer('Semadforo',tracking=True,default=3 )
-    light_help = fields.Integer('Semadforo temo',tracking=True,default=3, compute='_compute_show_light' )
+    light = fields.Integer('Semadforo',tracking=True )
+    light_help = fields.Integer('Semadforo temo',tracking=True,default=0, compute='_compute_show_light' )
     #ON BUTTON ACTIONS
 
     #ON COMPUTE

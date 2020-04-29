2a80f97c78675814387b4adc36a304455b1ac150
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-29 17:33:23 -0500

    add id in project

--- light_control/static/src/js/tomcat_sale_order_line.js
+++ light_control/static/src/js/tomcat_sale_order_line.js
@@ -53,7 +53,7 @@ odoo.define('light_control.tomcat_sale_order_line', function (require) {
                     if (this.addTrashIcon) {
                         nbrColumns--;
                     }
-                    $cell.attr('colspan', (nbrColumns-1));
+                    $cell.attr('colspan', (nbrColumns));
                   
                 }
                
@@ -90,7 +90,7 @@ odoo.define('light_control.tomcat_sale_order_line', function (require) {
             var self = this;
             return this._super.apply(this, arguments).then(function () {
                 self.$('.o_list_table').addClass('o_section_and_note_list_view');
-                self.$('.o_list_table').find("[data-name='x_extra']").toggle();
+                self.$('.o_list_table').find("thead [data-name='x_extra']").toggle();
             });
         }
     });

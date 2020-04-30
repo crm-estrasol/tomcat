837a05e0f989f5ad00468eddf7d3ac48714fc9b3
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-30 13:08:45 -0500

    orders lines adjustment

--- tomcat/models/sales/sale_order.py
+++ tomcat/models/sales/sale_order.py
@@ -37,8 +37,9 @@ class TomCatSaleOrder(models.Model):
             body += "<p> Nuevo(s) </p>" if news_l > 0 else ""
             for new in  news:
                 if "project_sections" in  new[2]:
-                    id_proy = new[2]['project_sections']   
-                    new_name = self.env['tomcat.project.section'].search([('id','=',id_proy )])[0].name   
+                    if new[2]['project_sections'] > 0:
+                        id_proy = new[2]['project_sections']   
+                        new_name = self.env['tomcat.project.section'].search([('id','=',id_proy )])[0].name   
                 else:    
                     new_name = new[2]['name'] if  'name' in  new[2]  else "Sin cambio"
                 new_qty = new[2]['product_uom_qty'] if  'product_uom_qty' in  new[2]  else "Sin cambio"

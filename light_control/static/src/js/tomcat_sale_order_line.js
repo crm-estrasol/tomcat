fafd9ba101ac32181411f60781228f330fcd1b2a
Kevin del Campo <kcampo@estrasol.com.mx>
2020-04-30 11:39:52 -0500

    orders lines adjustment

--- light_control/static/src/js/tomcat_sale_order_line.js
+++ light_control/static/src/js/tomcat_sale_order_line.js
@@ -37,7 +37,7 @@ odoo.define('light_control.tomcat_sale_order_line', function (require) {
                 return $cell.addClass('o_hidden');
             }
             
-            if( isProject && !node.attrs.name === "sequence"){
+            if( isProject && node.attrs.name !== "sequence"){
                
                 return $cell.addClass('o_hidden').attr('colspan',0);
                 

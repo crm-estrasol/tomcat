f400f2757ebdb2723731c34ad9f01b6e21f6402d
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-01 10:49:11 -0500

    map test

---
+++ open_map/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Light controller",  # Module title
+    'summary': "Manage light controller",  # Module subtitle phrase
+    'description': """Long description""",  # You can also rst format
+    'author': "Estrasol -Kevin Daniel del Campo",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Uncategorized',
+    'version': '12.0.1',
+    'depends': ['base', 'web','account'],
+    'data': [
+      
+        'views/templates.xml',
+    ]
+}

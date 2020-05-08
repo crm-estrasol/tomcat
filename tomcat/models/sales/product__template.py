8dcbc64176bdef91de77b68061c024c995581fee
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-08 15:26:46 -0500

    modificar producto concepto modelo

--- tomcat/models/sales/product__template.py
+++ tomcat/models/sales/product__template.py
@@ -6,5 +6,9 @@ from odoo.tools.misc import clean_context
 class TomcatProductTemplate(models.Model):
     _inherit = "product.template"
     name = fields.Char('Modelo', index=True, required=True, translate=True)
+    client_model = fields.Char('Modelo cliente', index=True, required=True)
+    #brand = fields.Char('Marca', index=True, required=True)
+    
+
 
 

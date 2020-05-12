46ac3074c935bf84939d18a0e1ebb301d15e93e5
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-12 09:14:46 -0500

    modificar producto concepto modelo

--- tomcat/models/sales/sale_order_line.py
+++ tomcat/models/sales/sale_order_line.py
@@ -24,7 +24,7 @@ class TomCatSaleOrderLine(models.Model):
     
     project_sections = fields.Many2one('tomcat.project.section', string='Proyecto',track_visibility=True)
     type_proyect  = fields.Selection(related='product_id.service_tracking') 
-    margin_tomcat = fields.Char("Nombre")
+    margin_tomcat = fields.Char("Margen(%)")
     marigin_tTotal  = fields.Char("Nombre")
 
      
@@ -58,14 +58,16 @@ class TomCatSaleOrderLine(models.Model):
             margin_tomcat="self.product_uom.id",
            
         )
-        vals['margin_tomcat'] = "d"
-        vals['margin_tomcat'] = "self.product_uom_qty or 1.0"
+        vals['margin_tomcat'] = self.product_id.margin_ut
+       
         vals.update(name=self.get_sale_order_line_multiline_description_sale(product))
 
         self._compute_tax_id()
 
         if self.order_id.pricelist_id and self.order_id.partner_id:
             vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+            vals['price_unit'] =  vals['price_unit']  / (1 -  self.product_id.margin_ut ) 
+        
         self.update(vals)
 
         title = False

4c842c3bebad301c6703a5ed7399f8dcea720d24
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-12 10:02:09 -0500

    modificar producto concepto modelo

--- tomcat/models/sales/sale_order_line.py
+++ tomcat/models/sales/sale_order_line.py
@@ -68,7 +68,7 @@ class TomCatSaleOrderLine(models.Model):
             vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
             _logger.info("-----------------------------------"+str( vals['price_unit'] ) )
 
-            vals['price_unit'] =  vals['price_unit']  / (1 -  self.product_id.m ) 
+            vals['price_unit'] =  vals['price_unit']  / (1 -  self.product_id.margin_ut ) 
             _logger.info("-----------------------------------"+str( vals['price_unit'] ) )
         
         self.update(vals)

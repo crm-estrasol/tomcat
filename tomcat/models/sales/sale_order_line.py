5bc797edb64b9f0e90e6999451292d8c9375f8e9
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-12 15:41:14 -0500

    modificar producto concepto modelo

--- tomcat/models/sales/sale_order_line.py
+++ tomcat/models/sales/sale_order_line.py
@@ -71,7 +71,7 @@ class TomCatSaleOrderLine(models.Model):
             value =  self.env['product.pricelist.item'].search(['id','=',id_rule])[0]
             #_logger.info("-----------------------------------"+str(value) )
             vals['margin_tomcat'] = value.margin_ut
-            vals['price_unit'] =  vals['price_unit']  / (1 -  vals['margin_tomcat'] ) 
+            #vals['price_unit'] =  vals['price_unit']  / (1 -  vals['margin_tomcat'] ) 
             
           
         
@@ -147,7 +147,7 @@ class TomCatSaleOrderLine(models.Model):
                 value =  self.env['product.pricelist.item'].search(['id','=',id_rule])[0]
                 _logger.info("-----------------------------------"+str(value) )
                 self.margin_tomcat = value.margin_ut
-                self.price_unit = self.price_unit  / (1 -  self.margin_tomcat ) 
+               # self.price_unit = self.price_unit  / (1 -  self.margin_tomcat[0].margin_ut ) 
     def _get_display_rule(self, product):
                 # TO DO: move me in master/saas-16 on sale.order
                 # awa: don't know if it's still the case since we need the "product_no_variant_attribute_value_ids" field now

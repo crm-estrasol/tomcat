9721c8db0fac827775005f9f1e8f27fa0c93f024
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-18 12:36:04 -0500

    unrestrict fields

--- tomcat/models/sales/sale_order.py
+++ tomcat/models/sales/sale_order.py
@@ -254,7 +254,7 @@ class TomCatSaleOrder(models.Model):
                 'views':  [(view_id,'form')],
                 'type': 'ir.actions.act_window',
                 'target': 'new',
-                'domain':[{   'project_ids': [('id', 'in', sistemas )] }]
+                'context':{'default_sale':self.id}
                 
        
            

250c8e4e6d08deb2ab8973ff97e7b1bc6fc74bcb
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-20 16:20:39 -0500

    report

--- tomcat/__manifest__.py
+++ tomcat/__manifest__.py
@@ -45,8 +45,10 @@
         'views/templates/templates.xml',
         
         #'views/sales/sales_cron.xml',
-        'views/portal/sale/sale_order.xml',
+        #'views/portal/sale/sale_order.xml',
         'reports/sales_report.xml',
+         'reports/sales_report_sistems.xml',
+        
         'reports/sale_report_prueba.xml', 
 
         'reports/reports.xml',

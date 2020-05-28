7dc21bd515bd5b7d24e00cf411ebf3e85f791060
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-28 15:14:29 -0500

    excel

--- tomcat/models/sales/sale_order.py
+++ tomcat/models/sales/sale_order.py
@@ -510,7 +510,7 @@ class MailComposerTomcat(models.TransientModel):
         return {'value': values}
     """
 
-    
+    """
     @api.onchange('excel')
     def on_change_excel(self):
                 workbook = xlwt.Workbook(encoding='utf-8')
@@ -533,6 +533,26 @@ class MailComposerTomcat(models.TransientModel):
                 values['attachment_ids'] = [(6, 0, attachment_ids) ]
                 values = self._convert_to_write(values)
                 return {'value': values}
+    """
     @api.onchange('template_id')
-    def onchange_template_id_wrapper(self):
-        return 
+    def onchange_template_id_dos(self):
+                 workbook = xlwt.Workbook(encoding='utf-8')
+                worksheet = workbook.add_sheet('Testing')
+                worksheet.write_merge(0 , 0,  2, 5, "Cliente")
+                fp =  BytesIO()
+                workbook.save(fp)
+                values = {}
+                attachment_ids = []
+                Attachment = self.env['ir.attachment']
+            
+                data_attach = {
+                        'name': "cotz.xls",
+                        'datas': base64.encodestring( fp.getvalue()) ,
+                        'res_model': 'mail.compose.message',
+                        'res_id': 0,
+                        'type': 'binary',  # override default_type from context, possibly meant for another model!
+                }
+                attachment_ids.append(Attachment.create(data_attach).id)
+                values['attachment_ids'] = [(6, 0, attachment_ids) ]
+                values = self._convert_to_write(values)
+                return {'value': values}

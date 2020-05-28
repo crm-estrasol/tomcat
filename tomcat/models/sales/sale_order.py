d5579b3479ff70c263585f9ad72ff1011f357baf
Kevin del Campo <kcampo@estrasol.com.mx>
2020-05-28 13:46:13 -0500

    excel

--- tomcat/models/sales/sale_order.py
+++ tomcat/models/sales/sale_order.py
@@ -461,18 +461,18 @@ class TomCatSaleOrder(models.Model):
         worksheet.write_merge(0 , 0,  2, 5, "Cliente")
         fp =  BytesIO()
         workbook.save(fp)
-        attachment_ids = []
-        Attachment = self.env['ir.attachment']
         
+        Attachment = self.env['ir.attachment']
+            
         data_attach = {
-                    'name': "cotz.xls",
-                    'datas': base64.encodestring( fp.getvalue()) ,
-                    'res_model': 'mail.compose.message',
-                    'res_id': 0,
-                    'type': 'binary',  # override default_type from context, possibly meant for another model!
+                'name': "cotz.xls",
+                'datas': base64.encodestring( fp.getvalue()) ,
+                'res_model': 'mail.compose.message',
+                'res_id': 0,
+                'type': 'binary',  # override default_type from context, possibly meant for another model!
         }
         attachment_ids.append(Attachment.create(data_attach).id)
-        values['attachment_ids'] = [(6, 0, attachment_ids) ]
+        
         ctx['default_excel'] = True
         ctx['default_attachment_ids'] = [(6, 0, attachment_ids) ]
         values['context'] = ctx 
@@ -490,39 +490,45 @@ class SaleReport(models.Model):
 class MailComposerTomcat(models.TransientModel):
     _inherit = 'mail.compose.message'
     excel = fields.Boolean('Excel',default=False)
-    @api.onchange('excel')
-    def on_change_excel(self):
-        workbook = xlwt.Workbook(encoding='utf-8')
-        worksheet = workbook.add_sheet('Testing')
-        worksheet.write_merge(0 , 0,  2, 5, "Cliente")
-        fp =  BytesIO()
-        workbook.save(fp)
-        values = {}
-        values['attachment_ids'] =  [(6, 0,  ) ]
-        values = self._convert_to_write(values)
-        return {'value': values}
+    #@api.onchange('excel')
+    #def on_change_excel(self):
+    #    workbook = xlwt.Workbook(encoding='utf-8')
+    #    worksheet = workbook.add_sheet('Testing')
+    #    worksheet.write_merge(0 , 0,  2, 5, "Cliente")
+    #    fp =  BytesIO()
+    #    workbook.save(fp)
+    #    values = {}
+    #    values['attachment_ids'] =  [(0, 0,  
+    #                                  { 
+    #                                    'name': "cotz.xls",
+    #                                    'store_fname':"cotz.xls",
+    #                                    'datas':base64.encodestring(fp.getvalue()) 
+    #                                  }
+    #                                                                  ) ]
+    #    values = self._convert_to_write(values)
+    #    return {'value': values}
 
         """
         @api.onchange('excel')
         def on_change_excel(self):
-            workbook = xlwt.Workbook(encoding='utf-8')
-            worksheet = workbook.add_sheet('Testing')
-            worksheet.write_merge(0 , 0,  2, 5, "Cliente")
-            fp =  BytesIO()
-            workbook.save(fp)
-            values = {}
-            attachment_ids = []
-            Attachment = self.env['ir.attachment']
-        
-            data_attach = {
-                    'name': "cotz.xls",
-                    'datas': base64.encodestring( fp.getvalue()) ,
-                    'res_model': 'mail.compose.message',
-                    'res_id': 0,
-                    'type': 'binary',  # override default_type from context, possibly meant for another model!
-            }
-            attachment_ids.append(Attachment.create(data_attach).id)
-            values['attachment_ids'] = [(6, 0, attachment_ids) ]
-            values = self._convert_to_write(values)
-            return {'value': values}
+                workbook = xlwt.Workbook(encoding='utf-8')
+                worksheet = workbook.add_sheet('Testing')
+                worksheet.write_merge(0 , 0,  2, 5, "Cliente")
+                fp =  BytesIO()
+                workbook.save(fp)
+                values = {}
+                attachment_ids = []
+                Attachment = self.env['ir.attachment']
+            
+                data_attach = {
+                        'name': "cotz.xls",
+                        'datas': base64.encodestring( fp.getvalue()) ,
+                        'res_model': 'mail.compose.message',
+                        'res_id': 0,
+                        'type': 'binary',  # override default_type from context, possibly meant for another model!
+                }
+                attachment_ids.append(Attachment.create(data_attach).id)
+                values['attachment_ids'] = [(6, 0, attachment_ids) ]
+                values = self._convert_to_write(values)
+                return {'value': values}
         """

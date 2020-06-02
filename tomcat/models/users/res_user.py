d0b693ee7e1b00fa2af6fcd2e5c951c2bac5685b
Kevin del Campo <kcampo@estrasol.com.mx>
2020-06-02 11:26:34 -0500

    filter user

--- tomcat/models/users/res_user.py
+++ tomcat/models/users/res_user.py
@@ -4,17 +4,20 @@ _logger = logging.getLogger(__name__)
 from odoo.tools.misc import clean_context
 class TomcatResUser(models.Model):
     _inherit = "res.users"
-    def _accion(self):
-        self.env.cr.execute("SELECT * FROM table_search_partners")
-
-        vals = self._cr.dictfetchall()
-        array = [ x['user_id'] for x in vals if  x['partner_id'] != self.id  ]
-        
-        return [('customer_rank','>', 0),('id','not in', array )]
     partner_avaible  =  fields.Many2many(comodel_name='res.partner', relation='table_search_partners', 
     column1='partner_id', 
     column2='user_id',
-    domain= _accion  )
+      )
+    partner_Navaible  =  fields.Many2many(comodel_name='res.partner', relation='table_search_partemp', 
+    column1='partner_id', 
+    column2='user_id',
+    compute='_accion'
+      )
 
-    
-    
+    def _accion(self):
+        for item in self:
+            self.env.cr.execute("SELECT * FROM table_search_partners")
+            vals = self._cr.dictfetchall()
+            array = [ x['user_id'] for x in vals if  x['partner_id'] != item.id  ]
+            item.partner_Navaible = [ (4,x.id ) for x in array ]  
+        

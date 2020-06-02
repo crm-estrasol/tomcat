65bfd88a7a38453b885d9c473e0143e8a8df3d70
Kevin del Campo <kcampo@estrasol.com.mx>
2020-06-02 10:58:44 -0500

    filter user

--- tomcat/models/users/res_user.py
+++ tomcat/models/users/res_user.py
@@ -7,7 +7,7 @@ class TomcatResUser(models.Model):
     partner_avaible  =  fields.Many2many(comodel_name='res.partner', relation='table_search_partners', 
     column1='partner_id', 
     column2='user_id',
-    domain=lambda self: [ ('customer_rank','>', 0),('id','not in', self.accion() )]  )
+    domain= [('customer_rank','>', 0),('id','not in', self.accion() )]  )
 
     def accion(self):
         self.env.cr.execute("SELECT * FROM table_search_partners")

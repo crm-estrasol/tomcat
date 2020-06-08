faecb3874fc943468f35875ccf0eefb3d83e4c96
Kevin del Campo <kcampo@estrasol.com.mx>
2020-06-08 11:29:48 -0500

    PO user

--- tomcat/wizard/sale_details.py
+++ tomcat/wizard/sale_details.py
@@ -13,6 +13,7 @@ class SaleDiscount(models.TransientModel):
     projects =  fields.Many2many(comodel_name='tomcat.project', relation='table_many_project_2', column1='project_id', column2='',string="Sistema")
     ubications =  fields.Many2many(comodel_name='tomcat.ubication', relation='table_many_ubication_2', column1='ubication_id', column2='',string="Ubicaciones")
     brand =  fields.Many2many(comodel_name='tomcat.brand', relation='table_many_brand', column1='brand_id', column2='',string="Marcas")
+    partner =  fields.Many2many(comodel_name='res..partner', relation='table_many_partner', column1='partner_id', column2='',string="Proveedor")
     discount =  fields.Float("Descuento",digits=(16, 2) )
     sale = fields.Many2one('sale.order', string='Venta')
 
@@ -25,9 +26,16 @@ class SaleDiscount(models.TransientModel):
         sistemas = [item.project.id for item in self.sale.order_line if item.product_id and item.project  ]
         marcas = [item.product_id.brand.id for item in self.sale.order_line if item.product_id and item.product_id.brand  ]
         ubicaciones = [ item.ubication.id for item in self.sale.order_line if item.product_id and item.ubication  ]
+        sellers = []
+        for item in self.sale.order_line: 
+            if item.product_id and item.ubication:
+                for seller in item.product_id.seller_ids:
+                    sellers.append(seller.name.id) 
+        sellers = set(sellers)
         return {
             'domain': { 'projects': [('id', 'in', sistemas)], 
                         'ubications': [('id', 'in', ubicaciones)],
                         'brand': [('id', 'in', marcas)],
+                        'partner': [('id', 'in', sellers)],
                       }                     
         }

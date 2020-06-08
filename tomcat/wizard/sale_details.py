caf582fb820ab5168560f45a8d6e60e22ce82bda
Kevin del Campo <kcampo@estrasol.com.mx>
2020-06-08 11:34:07 -0500

    PO user

--- tomcat/wizard/sale_details.py
+++ tomcat/wizard/sale_details.py
@@ -13,7 +13,7 @@ class SaleDiscount(models.TransientModel):
     projects =  fields.Many2many(comodel_name='tomcat.project', relation='table_many_project_2', column1='project_id', column2='',string="Sistema")
     ubications =  fields.Many2many(comodel_name='tomcat.ubication', relation='table_many_ubication_2', column1='ubication_id', column2='',string="Ubicaciones")
     brand =  fields.Many2many(comodel_name='tomcat.brand', relation='table_many_brand', column1='brand_id', column2='',string="Marcas")
-    partner =  fields.Many2many(comodel_name='res..partner', relation='table_many_partner', column1='partner_id', column2='',string="Proveedor")
+    partner =  fields.Many2many(comodel_name='res.partner', relation='table_many_partner', column1='partner_id', column2='',string="Proveedor")
     discount =  fields.Float("Descuento",digits=(16, 2) )
     sale = fields.Many2one('sale.order', string='Venta')
 

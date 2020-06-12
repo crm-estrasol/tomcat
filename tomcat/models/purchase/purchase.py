d84058678f6da98f8b97d664054c43b75f635a80
Kevin del Campo <kcampo@estrasol.com.mx>
2020-06-12 17:50:29 -0500

    a

--- tomcat/models/purchase/purchase.py
+++ tomcat/models/purchase/purchase.py
@@ -18,12 +18,21 @@ class TomcatPurchase(models.Model):
 class TomcatPurchaseLine(models.Model):
     _inherit = "purchase.order.line"
     discount = fields.Float(help="Discount",digits=(16, 2) ,store=True)
-    @api.onchange('product_qty', 'product_uom')
-    def _onchange_quantity(self):
-        super(TomcatPurchaseLine, self)._onchange_quantity()
-        
-    @api.onchange('discount')    
-    def apply_discount(self):
-        if self.discount > 0:
-            self.price_subtotal =  self.price_subtotal  - (self.price_subtotal * (self.discount / 100) )
+ 
+    @api.depends('product_qty', 'price_unit', 'taxes_id')
+    def _compute_amount(self):
+        for line in self:
+            vals = line._prepare_compute_all_values()
+            taxes = line.taxes_id.compute_all(
+                vals['price_unit'],
+                vals['currency_id'],
+                vals['product_qty'],
+                vals['product'],
+                vals['partner'])
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'] * 2,
+            })
+    
     

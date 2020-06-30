ab1dbdf50b9454fcc3e8f35b2242d4a8f30fa0bd
Kevin del Campo <kcampo@estrasol.com.mx>
2020-06-30 13:06:36 -0500

    imagen

--- tomcat/models/sales/sale_order.py
+++ tomcat/models/sales/sale_order.py
@@ -570,7 +570,7 @@ class MailComposerTomcat(models.TransientModel):
         img = Image.open('/home/odoo/src/user/tomcat/static/src/img/colocaralcentro2.png')
         r, g, b, a = img.split()
         img = Image.merge("RGB", (r, g, b))
-        img.thumbnail((150,150), Image.ANTIALIAS)
+        img.thumbnail((152,152), Image.ANTIALIAS)
         img = img.save('colocarcentro.bmp')
         worksheet.insert_bitmap('colocarcentro.bmp', 0,0,50,0)
         worksheet.merge(0,8,0,1)

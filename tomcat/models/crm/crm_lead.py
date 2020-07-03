80f13703a43d13e69fb3d27e6c705e297bcf078c
Kevin del Campo <kcampo@estrasol.com.mx>
2020-07-03 16:06:06 -0500

    a

--- tomcat/models/crm/crm_lead.py
+++ tomcat/models/crm/crm_lead.py
@@ -12,7 +12,7 @@ class TomCatCrmLead(models.Model):
     _inherit  = "crm.lead"
     light = fields.Integer('Semaforo',tracking=True,default=0 )
     light_help = fields.Integer('Semadforo temo',default=0, compute='_compute_show_light' )
-    partners_ted =  fields.Many2many(comodel_name='res.partner', relation='table_mny_partner', column1='partner_id', column2='',string="Usarios")
+    partners_related =  fields.Many2many(comodel_name='res.partner', relation='table_mny_partner', column1='partner_id', column2='',string="Usarios")
     filter_users = fields.One2many(related="partner_id.child_ids")
     
     #extra_partner =  fields.Many2many(comodel_name='res.partner', relation='tabl_mny_partner', column1='partner_id', column2='',string="Usarios")
